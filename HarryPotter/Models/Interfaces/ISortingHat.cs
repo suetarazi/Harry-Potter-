075f993281632baced5bb03bbabb82bbb99f8477
Sue Tarazi <suemachtley@gmail.com>
2020-09-20 22:31:24 -0700

    sorting hat interface and basic service

---
+++ HarryPotter/Models/Interfaces/ISortingHat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Models.Interfaces
+{
+    interface ISortingHat
+    {
+        public Task SortingHatOutcome();
+    }
+}
