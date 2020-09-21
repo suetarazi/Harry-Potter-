bf4a0b4d98b2b3fb58ac561156745455dc9b3af1
Sue Tarazi <suemachtley@gmail.com>
2020-09-20 22:05:18 -0700

    Add project files.

---
+++ HarryPotter/Models/SortingHat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Models
+{
+    public class SortingHat
+    {
+        public string HouseName { get; set; }
+    }
+}
