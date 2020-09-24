080d2299d471dd644bfd1a36705c1f61fa1dbf82
Sue Tarazi <suemachtley@gmail.com>
2020-09-24 16:04:19 -0700

    built MVC for SortingHat, SortingHat service and ISortingHat interface

---
+++ HarryPotter/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HarryPotter.Models;
+using HarryPotter.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarryPotter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISortingHat _sortingHat;
+
+        public HomeController(ISortingHat sortingHat)
+        {
+            _sortingHat = sortingHat;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string submit)
+        {
+            SortingHat sorting = await _sortingHat.SortingHatOutcome();
+            return View(sorting);
+        
+        }
+    }
+}
