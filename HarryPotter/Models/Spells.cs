ea08296de951d6ffc9078f0f62f7664e1afd1b0a
Sue Tarazi <suemachtley@gmail.com>
2020-09-28 18:51:47 -0700

    spell model

---
+++ HarryPotter/Models/Spells.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Models
+{
+    public class Spells
+    {
+        [JsonPropertyName("_id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("spell")]
+        public string SpellName { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("effect")]
+        public string Effect { get; set; }
+
+        [JsonPropertyName("__v")]
+        public string V { get; set; }
+
+
+    }
+}
